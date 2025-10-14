35d6d66d912abdb8fbf4fe11b326c36040e2e16f
Giga Chaand <arunmondal9286@gmail.com>
2025-10-14 06:23:56 +0530

    Update uninstall.sh

--- revanced-magisk/uninstall.sh
+++ revanced-magisk/uninstall.sh
@@ -3,7 +3,7 @@
 	MODDIR=${0%/*}
 	. "$MODDIR/config"
 
-	rm "/data/adb/rvpnm/${MODDIR##*/}.apk"
-	rmdir "/data/adb/rvpnm"
+	rm "/data/adb/rvgc/${MODDIR##*/}.apk"
+	rmdir "/data/adb/rvgc"
 	rm "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
 } &
